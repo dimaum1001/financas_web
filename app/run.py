8169939e2339c1b9d5f005eaf9584156505a792c
Dimas <dimas_cdasilva@yahoo.com.br>
2025-07-16 16:14:03 -0300

    quarto commit mudança do run.py

--- app/run.py
+++ app/run.py
@@ -1,5 +1,5 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8001, reload=True)
-
+    uvicorn.run("app.main:app", host="127.0.0.1", port=int(os.environ.get("PORT", 8000)))
