db3d86c95c18027cd4624ffab4ea4da79fac9f3e
Dimas <dimas_cdasilva@yahoo.com.br>
2025-07-17 10:44:25 -0300

    9 commit ajuste front e back

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 import uvicorn
+import os
+##if __name__ == "__main__":
+  ##  uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+    port = int(os.environ.get("PORT", 10000))  # 10000 é o valor padrão local
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
 
