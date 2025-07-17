0c02b9e2af7fc2eb1adbd1d9747dd7d9fb285081
Dimas <dimas_cdasilva@yahoo.com.br>
2025-07-17 11:24:33 -0300

    atualizar ap.js para o link do render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: "http://localhost:10000", // backend FastAPI
+   baseURL: "https://financas-web-19.onrender.com", // backend FastAPI
 });
 
 export default api;
